0b712528f13696f5cb3e907543e037105df63ba6
Tonycs <lam.tony540@gmail.com>
2026-02-12 17:54:54 -0500

    Fixed 404 page bug

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react"
 import { Helmet } from "react-helmet-async";
 import ReactPlayer from 'react-player';
 import Header from '../Header/Header.jsx'
+import FiveStar from '../FiveStar/FiveStar.jsx'
 
 const VideoPlayer = () => {
   const { id } = useParams()
@@ -29,42 +30,58 @@ const VideoPlayer = () => {
     return <p>Loading...</p>
   }
 
-  const title = video ? video.title : ""
+  if (!video) {
+    return <div className={styles.pageNotFound}>
+      <h1 className={styles.error}>404</h1>
+      <h2 className={styles.errorDesc}>Page not found</h2>
+      <p className={styles.errorMessage}>Sorry, the page you are looking for does not exist or an error has occured. Please go back, or head to our homepage at evergreenvideos.com.</p>
+    </div>
+  }
 
   return (
     <>
       <Helmet>
-        <title>{title} | Evergreen</title>
+        <title>{video.title} | Evergreen</title>
       </Helmet>
 
+      <Header />
       <div className={styles.container}>
-        <Header />
-
         {!video && <div className={styles.pageNotFound}>
           <h1 className={styles.error}>404</h1>
           <h2 className={styles.errorDesc}>Page not found</h2>
           <p className={styles.errorMessage}>Sorry, the page you are looking for does not exist or an error has occured. Please go back, or head to our homepage at evergreenvideos.com.</p>
         </div>}
 
-        <ReactPlayer
-          src={`https://www.youtube.com/watch?v=${id}`}
-          autoPlay={1}
-          controls
-          width="75%"
-          height="75%"
-          onError={(e) => console.log('ReactPlayer Error:', e)}
+        <div className={styles.mainContent}>
+          <div className={styles.videoHeader}>
+            <h1 className={styles.videoTitle}>{video.title}</h1>
+            <div className={styles.metadata}>
+              <p className={styles.channel_name}>{video.channel_name}</p>
+              <p className={styles.publish_date}>{video.publish_date}</p>
+              <p className={styles.descLink}>Read description</p>
+            </div>
+          </div>
 
-          config={{
-            youtube: {
-              playerVars: {
-                modestbranding: 1,
-                rel: 0,
-                showinfo: 0,
-                iv_load_policy: 3
-              }
-            }
-          }}
-        />
+          <div className={styles.videoContainer}>
+            <ReactPlayer
+              src={`https://www.youtube.com/watch?v=${id}`}
+              autoPlay={1}
+              controls
+              width={"100%"}
+              height={"100%"}
+              onError={(e) => console.log('ReactPlayer Error:', e)}
+              className={styles.embeddedVideo}
+            />
+          </div>
+
+          <div className={styles.actionRow}>
+            <div className={styles.rating}>
+              <h2 className={styles.ratingText}>Rate this video essay</h2>
+              <FiveStar />
+            </div>
+            <button>Write a review</button>
+          </div>
+        </div>
       </div >
     </>
   )
